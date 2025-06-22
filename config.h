1798461e5cf176b750fd18d49926d0b8abd3270d
danahparker <danaharmonparker@gmail.com>
2025-06-21 17:00:39 -0700

    add font adjustment with alt shift + and alt shift _

--- config.h
+++ config.h
@@ -186,6 +186,7 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define ALTSHIFTMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -193,8 +194,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ ALTSHIFTMOD,          XK_plus,        zoom,           {.f = +1} }, // Alt + Shift + plus
+	{ ALTSHIFTMOD,          XK_underscore,  zoom,           {.f = -1} }, // Alt + Shift + underscore
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
